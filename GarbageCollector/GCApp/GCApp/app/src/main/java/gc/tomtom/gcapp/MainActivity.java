ac13eb76e508160f03eceaa33e799d73c6b10fc0
Nilamber <bittusavarn@gmail.com>
2018-11-19 02:26:48 +0530

    commit android app

---
+++ GarbageCollector/GCApp/GCApp/app/src/main/java/gc/tomtom/gcapp/MainActivity.java
@@ -0,0 +1,205 @@
+package gc.tomtom.gcapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.nfc.Tag;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    WebView webView;
+    String TAG = MainActivity.class.getSimpleName();
+    Location location;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            public void onPageFinished(WebView view, String url) {
+                Log.d(TAG,"Page fininshed" +location.getLatitude()+ location.getLongitude());
+                webView.loadUrl("javascript:callJS();");
+            }
+        });
+        WebSettings ws = webView.getSettings();
+
+        ws.setJavaScriptEnabled(true);
+        ws.setAllowFileAccess(true);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            try {
+                Log.d(TAG, "Enabling HTML5-Features");
+                Method m1 = WebSettings.class.getMethod("setDomStorageEnabled", new Class[]{Boolean.TYPE});
+                m1.invoke(ws, Boolean.TRUE);
+
+                Method m2 = WebSettings.class.getMethod("setDatabaseEnabled", new Class[]{Boolean.TYPE});
+                m2.invoke(ws, Boolean.TRUE);
+
+                Method m3 = WebSettings.class.getMethod("setDatabasePath", new Class[]{String.class});
+                m3.invoke(ws, "/data/data/" + getPackageName() + "/databases/");
+
+                Method m4 = WebSettings.class.getMethod("setAppCacheMaxSize", new Class[]{Long.TYPE});
+                m4.invoke(ws, 1024 * 1024 * 8);
+
+                Method m5 = WebSettings.class.getMethod("setAppCachePath", new Class[]{String.class});
+                m5.invoke(ws, "/data/data/" + getPackageName() + "/cache/");
+
+                Method m6 = WebSettings.class.getMethod("setAppCacheEnabled", new Class[]{Boolean.TYPE});
+                m6.invoke(ws, Boolean.TRUE);
+
+                Log.d(TAG, "Enabled HTML5-Features");
+            } catch (NoSuchMethodException e) {
+                Log.e(TAG, "Reflection fail", e);
+            } catch (InvocationTargetException e) {
+                Log.e(TAG, "Reflection fail", e);
+            } catch (IllegalAccessException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+        }
+        webView.loadUrl("http://de9a4b15.ngrok.io");
+        checkLocationPermission();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    LocationManager locationManager;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            String provider = locationManager.getBestProvider(new Criteria(), true);
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+            location = locationManager.getLastKnownLocation(provider);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        String provider = locationManager.getBestProvider(new Criteria(), true);
+
+                        locationManager.requestLocationUpdates(provider, 400, 1, this);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission")
+                        .setMessage("Permisiion Required")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+      Log.d(TAG,"location is" + location.getLatitude() +","+ location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+}
